06365cd0c19951ba4af1a44c2a8cccb92b1e1b81
codeaid-dev <codeaid.lab@gmail.com>
2023-08-18 22:16:11 +0900

    add fix

--- circle_move_mouse2.py
+++ circle_move_mouse2.py
@@ -2,12 +2,17 @@ import pygame as pg, sys
 
 pg.init()
 screen = pg.display.set_mode((500,500))
-pg.display.set_caption('円をマウスで動かす')
+pg.display.set_caption('マウスのボタンを取得する')
+font = pg.font.Font(None, 50)
 
 while True:
     screen.fill(pg.Color('white'))
     x,y = pg.mouse.get_pos()
 
+    b1, b2, b3 = pg.mouse.get_pressed()
+    text = font.render(f"{b1}:{b2}:{b3}", True, pg.Color('red'))
+    width,height = text.get_size()
+    screen.blit(text,((500-width)/2,(500-height)/2))
     pg.draw.circle(screen, pg.Color('black'), (x,y), 25)
     pg.display.update()
     pg.time.Clock().tick(60)
