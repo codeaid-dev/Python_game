ace0c938fb2b010f7078fecb9d39a52df9af5157
codeaid-dev <codeaid.lab@gmail.com>
2023-08-22 09:48:11 +0900

    fix add

--- drone_control1.py
+++ drone_control1.py
@@ -3,10 +3,10 @@ import pygame as pg, sys
 pg.init()
 screen = pg.display.set_mode((500,500))
 pg.display.set_caption('ドローンを操作する')
-img = pg.image.load('images/drone_red.png')
-width,height = img.get_size()
-img = pg.transform.scale(img,(width/3,height/3))
-width,height = img.get_size()
+drone = pg.image.load('images/drone_red.png')
+width,height = drone.get_size()
+drone = pg.transform.scale(drone,(width/3,height/3))
+width,height = drone.get_size()
 x,y = (500-width)/2,(500-height)/2
 speed = 5
 
@@ -31,7 +31,7 @@ while True:
     if y > 500:
         y = -height
 
-    screen.blit(img, (x,y))
+    screen.blit(drone, (x,y))
     pg.display.update()
     pg.time.Clock().tick(60)
     for event in pg.event.get():
--- drone_control3.py
+++ drone_control3.py
@@ -7,6 +7,7 @@ pg.init()
 screen = pg.display.set_mode((500,500))
 pg.display.set_caption('ドローンを操作する')
 drone = pg.image.load('images/drone_red.png')
+ufo = pg.image.load('images/ufo.png')
 width,height = drone.get_size()
 drone = pg.transform.scale(drone,(width/3,height/3))
 width,height = drone.get_size()
@@ -20,6 +21,20 @@ for i in range(100):
     target.dx = random.randint(1,4)
     target.dy = random.randint(1,4)
     targets.append(target)
+
+ufo_w,ufo_h = ufo.get_size()
+ufo = pg.transform.scale(ufo,(ufo_w/8,ufo_h/8))
+ufo_w,ufo_h = ufo.get_size()
+ufos = []
+for i in range(5):
+    u = Character()
+    u.rect = pg.Rect(random.randint(0,500-ufo_w),random.randint(0,500-ufo_h),ufo_w,ufo_h)
+    u.flag = False
+    u.dx = random.randint(1,4)
+    u.dy = random.randint(1,4)
+    ufos.append(u)
+
+clear,over = False,False
 
 while True:
     screen.fill(pg.Color('white'))
@@ -56,6 +71,32 @@ while True:
             if t.rect.y < 0 or t.rect.y > 490:
                 t.dy *= -1
             pg.draw.rect(screen, pg.Color('red'), t.rect)
+
+    for u in ufos:
+        if u.rect.colliderect(pr):
+            u.flag = True
+        if not u.flag:
+            u.rect.x += u.dx
+            u.rect.y += u.dy
+            if u.rect.x < 0 or u.rect.x > 500-u.rect.w:
+                u.dx *= -1
+            if u.rect.y < 0 or u.rect.y > 500-u.rect.h:
+                u.dy *= -1
+        screen.blit(ufo, u.rect)
+
+    flags = [u.flag for u in ufos]
+    if all(flags) and not clear:
+        font = pg.font.SysFont('Helvetica',30)
+        text = font.render('GAME OVER',True,(0,0,0))
+        screen.blit(text,((500-text.get_width())/2,(500-text.get_height())/2))
+        over = True
+
+    flags = [t.flag for t in targets]
+    if all(flags) and not over:
+        font = pg.font.SysFont('Helvetica',30)
+        text = font.render('GAME CLEAR',True,(0,0,0))
+        screen.blit(text,((500-text.get_width())/2,(500-text.get_height())/2))
+        clear = True
 
     pg.display.update()
     pg.time.Clock().tick(60)
