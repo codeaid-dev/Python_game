69f565875f208b41a8132dc85b4dc70f9aba9bdc
codeaid-dev <codeaid.lab@gmail.com>
2024-09-17 10:11:31 +0900

    add

---
+++ exercise3/missile_command1.py
@@ -0,0 +1,48 @@
+import pygame as pg, sys, math
+
+WIDTH,HEIGHT = 800,600
+pg.init()
+screen = pg.display.set_mode((WIDTH,HEIGHT))
+pg.display.set_caption('ミサイルコマンド')
+angle = 0
+missiles = []
+
+class Missile:
+    pass
+
+def target():
+    mx,my = pg.mouse.get_pos()
+    pg.draw.line(screen, (0,0,255), (mx-10,my), (mx+10,my), 5)
+    pg.draw.line(screen, (0,0,255), (mx,my-10), (mx,my+10), 5)
+
+def move_missile():
+    for m in missiles:
+        pg.draw.line(screen, (0,0,255), (WIDTH/2,HEIGHT-50), (m.x,m.y), 2)
+        pg.draw.circle(screen, (255,255,255), (m.x,m.y), m.radius)
+        if m.goalX-1!=int(m.x) and m.moving:
+            m.x += math.cos(m.angle)*0.2
+            m.y += math.sin(m.angle)*0.2
+        else:
+            m.moving = False
+
+while True:
+    screen.fill(pg.Color('black'))
+    target()
+    move_missile()
+    pg.display.update()
+    for event in pg.event.get():
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_SPACE:
+                m = Missile()
+                m.goalX,m.goalY = pg.mouse.get_pos()
+                m.x = WIDTH/2
+                m.y = HEIGHT-50
+                x = m.goalX-m.x
+                y = m.goalY-m.y
+                m.angle = math.atan2(y,x)
+                m.radius = 5
+                m.moving = True
+                missiles.append(m)
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
